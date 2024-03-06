5dfd12594b0655d40593897d8d9957a7a9974565
Randy Jeremy Rothschild <87795070+RandyJeremyRothschild-jpg@users.noreply.github.com>
2024-03-07 00:41:05 +0700

    commit2

--- resources/js/Components/Shared/SiteTitle.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export default function SiteTitle({style, className}) {
-    var className = `title ${className}`;
-    return <h1 className={className} style={{...style,textAlign: "left",fontWeight: "bold"}}>Perpustakaan<br/>Bersama</h1>
-}
